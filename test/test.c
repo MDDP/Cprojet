f4c8753ff3fd795f40e1d3c4ccac66206ae81488
LAMY-VERDIN Christophe <lamyverd@informatique.univ-paris-diderot.fr>
2018-03-22 12:39:49 +0100

    test.c: Ajout des configurations
    	Défini des commandes avec ctrl, cependant, il ne peuvent pas encore
    être modifiés.

--- test/test.c
+++ test/test.c
@@ -1,53 +1,165 @@
-#include <curses.h>
+#include <stdio.h>
+#include <ncurses.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#define CTRL(c) (c & 037)
+#define NBCONFIG 6
+
+void do_resize(int dummy){
+
+}
+
+void actualiseConfig(char control[]){
+	FILE *config=fopen("config.txt","r");
+	for(int i=0;i<NBCONFIG;i++){
+		char line[9];
+		fgets(line,9,config);
+		control[i]=line[strlen(line)-2];
+	}
+	fclose(config);
+}
 
 int main()
 {
+	WINDOW *haut, *bas;
 	initscr();			/* Start curses mode */
-	printw("Press '#' to exit\n");	/*        Exit       */
 	start_color();
 	//init_pair(1, COLOR_CYAN, COLOR_YELLOW);
 	keypad(stdscr,TRUE);
+    
 	noecho();
-	int cur_x=0, cur_y=0, maxcur_x=80, maxcur_y=100;
-	mvprintw(LINES - 1, 0, "Line %d, Column %d", cur_y, cur_x);
+    
+	int cur_x=0, cur_y=3, maxcur_x=80, maxcur_y=100;
+    haut = newwin(LINES-3, COLS, 3, 0);
+    scrollok(haut, TRUE);
+    //bas = newwin(3 , COLS, LINES - 3, 0); 
+    bas = subwin(stdscr, 3 , COLS, 0, 0); // Créé la même fenêtre que ci-dessus sauf que les coordonnées changent
+    box(bas, ACS_VLINE, ACS_HLINE);
+    mvwprintw(bas, 1, 1, "Press # to exit. Line %d, Column %d", cur_y-3, cur_x);
+    char control[NBCONFIG];
+    actualiseConfig(control);
+    raw();
+
+    wrefresh(haut);
+    wrefresh(bas);
+    
 	for(;;){
-		move(cur_y,cur_x);
+		move(cur_y, cur_x);
 		int ch = getch();
 		if(ch == '#') break;
+		if(ch == '*'){							//sauvegarde
+    		FILE *save=fopen("save.txt","w");	//crée un fichier save.txt qui va mettre
+    		putwin(haut,save);					//tout les caracteres introduit avec
+    		fclose(save);						//WPRINTW (ne marche pas avec addch chgat)
+    		break;
+		}
+		if(ch=='$'){							//charge le fichier save.txt
+            FILE *save=fopen("save.txt","r");
+			haut=getwin(save);
+    		fclose(save);
+    		wrefresh(haut);
+    		continue;
+		}
+		if(ch == CTRL(control[0])){
+			wprintw(haut,"copy");
+			continue;
+		}
+		if(ch == CTRL(control[1])){
+			wprintw(haut,"paste");
+			continue;
+		}
+		if(ch == CTRL(control[2])){
+			wprintw(haut,"cut");
+			continue;
+		}
+		if(ch == CTRL(control[3])){
+			move(cur_y,0);
+			clrtoeol();
+			cur_x=0;
+			continue;	
+		}
+		if(ch == CTRL(control[4])){
+			wprintw(haut,"menu");
+			continue;
+		}
+		if(ch == CTRL(control[5])){
+			/*
+			int newvalue=getch();
+			control[0]=newvalue;
+			*/
+			wprintw(haut,"refresh");
+			continue;
+		}
 		switch(ch){
 			case KEY_UP:
-				if(cur_y>0) cur_y-=1;
+				if(cur_y > 3) cur_y -= 1;
+                else if(cur_y == 3) wscrl(haut, -1);
 				break;
 			case KEY_DOWN:
-				if(cur_y<maxcur_y) cur_y+=1;
+				if(cur_y < maxcur_y) cur_y += 1;
+                if(cur_y-3 >= LINES-3){
+                    wscrl(haut, 1);
+                    move(LINES-1, cur_x);
+                }
 				break;
 			case KEY_LEFT:
-				if(cur_x>0) cur_x-=1;
+				if(cur_x > 0) cur_x -= 1;
+                if(cur_x == maxcur_x){
+                    cur_x = 0;
+                    cur_y++;
+                    move(cur_y, cur_x);
+                }
 				break;
 			case KEY_RIGHT:
 				if(cur_x<maxcur_x) cur_x+=1;
+                if(cur_x == 0) cur_y--;
 				break;
+            case 10 :   /* Touche Entrée */
+                if(cur_y < maxcur_y) {
+                    cur_x = 0;
+                    cur_y += 1;
+                    move(LINES-1, cur_x);
+                    wprintw(haut, "%c", '\n');
+                }
+                break;
 			case KEY_DC :
-				cur_x-=1;
 				delch();
 				break;
+			case 127:
+            case KEY_BACKSPACE :
+                if(cur_x == 0 && cur_y > 0){     /* Reposition du curseur à la ligne précédente,  */  
+                    cur_y -= 1;                  /* Pas encore de buffer donc on peut pas revenir */
+                   	cur_x = maxcur_x-1;			 /* au dernier caractère de la ligne précédente   */   
+                }
+                else
+                    cur_x-=1;
+                delch();
+                break;
+            case 410 :      /* Char qui apparait quand on redimensionne */
+                break;
 			default:
-				cur_x++;
-				if(cur_x==maxcur_x){
+                if(cur_y < maxcur_y && cur_x==maxcur_x){
 					cur_x=0;
 					cur_y++;
-				}
-				addch(ch | A_BOLD | A_UNDERLINE);
-				chgat(1, A_BOLD, 1, NULL);
-				mvprintw(LINES - 1, 0, "Line %d, Column %d", cur_y, cur_x);
-				break;
+                }
+                wmove(haut, cur_y-3, cur_x);
+				wprintw(haut, "%c",ch);
+                if(cur_x != COLS) cur_x+=1;
+                if(cur_y-3 >= LINES-3 && cur_x < maxcur_x) move(LINES-1, cur_x); /*Srolling du texte*/
+				break;     
 		}
-		
-
+        wclear(bas);
+        box(bas, ACS_VLINE, ACS_HLINE);
+		mvwprintw(bas, 1, 1, "Press # to exit. Line %d, Column %d", cur_y-3, cur_x);
+        wrefresh(haut);
+        wrefresh(bas);
 	}
-	refresh();			/* Print it on to the real screen */
-	endwin();			/* End curses mode		  */
+    
+	endwin();			/* End curses mode */
 
+    free(haut);
+    free(bas);
 	return 0;
 }
 
