4c17fdb5068f20502e175eccf948765f5c7d6e81
Mael Debois <mael.debs@gmail.com>
2018-04-17 16:40:27 +0200

    lisibilité

--- src/configuration.c
+++ src/configuration.c
@@ -1,11 +1,11 @@
 #include "configuration.h"
 
-void actualiseConfig(char control[]){
-  FILE *config=fopen("config.txt","r");
-  for(int i=0;i<NBCONFIG;i++){
+void actualiseConfig (char control[]) {
+  FILE *config = fopen("config.txt","r");
+  for(int i = 0; i < NBCONFIG; i++){
     char line[9];
     fgets(line,9,config);
-    control[i]=line[strlen(line)-2];
+    control[i] = line[strlen(line)-2];
   }
   fclose(config);
 }
