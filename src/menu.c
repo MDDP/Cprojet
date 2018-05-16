acb561ebb1dbebe5be1a9231713488b735f018e8
LAMY-VERDIN Christophe <chrislamyv@gmail.com>
2018-05-16 18:38:23 +0200

    menu.c: affichage dynamique mais quelques bugs

--- src/menu.c
+++ src/menu.c
@@ -1,14 +1,6 @@
 #include "menu.h"
-
-char *choices[] = {
-                        "Load          CTRL-L",
-                        "Save          CTRL-S",
-                        "Copy          CTRL-C",
-                        "Paste         CTRL-V",
-			"Cut           CTRL-X",
-			"Clear         CTRL-A",
-			"Refresh       CTRL-R",
-                  };
+#include <string.h>
+#include "configuration.h"
 
 int menu(){
 	ITEM **my_items;
@@ -17,6 +9,48 @@ int menu(){
 	int n_choices, i;
 	ITEM *cur_item;
 	
+	char control[NBCONFIG];
+	actualiseConfig(control);
+	
+	char *choice1 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice1, "Load          CTRL-");
+	choice1[strlen(choice1)] = toupper(control[0]);
+
+	char *choice2 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice2, "Save          CTRL-");
+	choice2[strlen(choice2)] = toupper(control[1]);
+
+	char *choice3 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice3, "\0");
+	strcat(choice3, "Copy          CTRL-");
+	choice3[strlen(choice3)] = toupper(control[2]);
+
+	char *choice4 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice4, "Paste         CTRL-");
+	choice4[strlen(choice4)] = toupper(control[3]);
+
+	char *choice5 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice5, "Cut           CTRL-");
+	choice5[strlen(choice5)] = toupper(control[4]);
+
+	char *choice6 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice6, "Clear         CTRL-");
+	choice6[strlen(choice6)] = toupper(control[5]);
+	
+	char *choice7 = (char*)calloc(1, 21 * sizeof(char));
+	strcpy(choice7, "Refresh       CTRL-");
+	choice7[strlen(choice7)] = toupper(control[6]);
+
+	char *choices[] = {
+        	choice1,
+        	choice2,
+        	choice3,
+        	choice4,
+		choice5,
+		choice6,
+		choice7,
+        };
+
 	
 	initscr();
 	cbreak();
@@ -24,7 +58,7 @@ int menu(){
 	keypad(stdscr, TRUE);
 
 	n_choices = ARRAY_SIZE(choices);
-	my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+	my_items = (ITEM **)calloc(n_choices+1, sizeof(ITEM *));
 
 	for(i = 0; i < n_choices; ++i)
 	        my_items[i] = new_item(choices[i], "");
@@ -59,6 +93,13 @@ int menu(){
 			
 		}
 	}
+	free(choice1);
+	free(choice2);
+	free(choice3);
+	free(choice4);
+	free(choice5);
+	free(choice6);
+	free(choice7);
 	free_item(my_items[0]);
 	free_item(my_items[1]);
 	free_menu(my_menu);
