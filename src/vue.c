9db1381bb6109237c26d6291faec12d859f0225d
LAMY-VERDIN Christophe <chrislamyv@gmail.com>
2018-05-18 17:54:23 +0200

    vue: nettoyage

--- src/vue.c
+++ src/vue.c
@@ -1,5 +1,60 @@
 #include "vue.h"
 
+//Initialise les fenêtres d'affichage
+WINDOW *haut, *bas;
+
+void getFilename (char* filename) {
+  for(int i=0; i<49; i++) filename[i] = '\0';
+  int compteur = 0; 
+  while(compteur<49) {
+    wclear(haut);
+    box(haut, ACS_VLINE, ACS_HLINE);
+    mvwprintw(haut, 1, 1, "Enter file name: %s", filename);
+    move(1, 18+compteur);
+    wrefresh(haut);
+    int ch = getch();
+    if(ch == 10) {
+      filename[compteur] = '\0';
+      break;
+    }
+    else if((ch == 127 || ch == KEY_BACKSPACE) && compteur>0) {
+      compteur--;
+      filename[compteur] = '\0';
+    }
+    else {
+      filename[compteur] = ch;
+      compteur++;
+    }
+  }
+}
+
+void afficheHautMessage (char* message) {
+  wclear(haut);
+  box(haut, ACS_VLINE, ACS_HLINE);
+  mvwprintw(haut, 1, 1, message);
+  wrefresh(haut);
+}
+
+void afficheHautMessageParameterStr (char* message, char* parameter) {
+  wclear(haut);
+  box(haut, ACS_VLINE, ACS_HLINE);
+  mvwprintw(haut, 1, 1, message, parameter);
+  wrefresh(haut);
+}
+
+void afficheHautMessageParameterInt (char* message, int parameter) {
+  wclear(haut);
+  box(haut, ACS_VLINE, ACS_HLINE);
+  mvwprintw(haut, 1, 1, message, parameter);
+  wrefresh(haut);
+}
+
+void afficheBufferBas (buffer *buff) {
+  wclear(bas);
+  wprintw(bas, "%s", buff->contenu);
+  wrefresh(bas);
+}
+
 void lancer () {
   // Démarrer le mode curses
   initscr();
@@ -18,9 +73,14 @@ void lancer () {
 
   //Initialise le buffer
   buffer *buff = initialisation(3000,80);
+
+  char tmp [2048];
+
+  int selection = -1;
+  int curChar = 0;
+  int first = 0;
+  int index = 0;
   
-  //Initialise les fenêtres d'affichage
-  WINDOW *haut, *bas;
   //Fenêtre contenant le buffer
   bas = newwin(LINES-3, COLS, 3, 0);
   //Fenêtre contenant les messages et les coordonées actuelles du curseur
@@ -35,14 +95,10 @@ void lancer () {
   wrefresh(bas);
   wrefresh(haut);
   init_pair(1, COLOR_CYAN, COLOR_BLACK);
-  int selection = -1;
-  int curChar = 0;
-  int first = 0;
-  char tmp [2048];
-  int index = 0;
+  
   //taille de la ligne en fonction de la fenetre
   if(COLS >= buff->t_ligne){
-	modifierTaille(COLS, buff);
+    modifierTaille(COLS, buff);
   }
   //Boucle principale
   for(;;){
@@ -52,193 +108,118 @@ void lancer () {
     if(ch == '#') break;
 
     else if(ch == CTRL(control[7])){
-	selection = menu();
-	actualiseConfig(control);
-	wclear(haut);
-	wclear(bas);
-	box(haut, ACS_VLINE, ACS_HLINE);
-      	mvwprintw(haut, 1, 1, "Menu loaded selection %d", selection);
-      	wrefresh(haut);
-	wprintw(bas, "%s", buff->contenu);
-	wrefresh(bas);
+      selection = menu();
+      actualiseConfig(control);
+      afficheHautMessageParameterInt("Menu loaded selection %d", selection);
+      afficheBufferBas(buff);
       continue;
     }
     else if(ch == CTRL(control[6])){
-	wclear(haut);
-      	box(haut, ACS_VLINE, ACS_HLINE);
-    	mvwprintw(haut, 1, 1, "Refresh");
-     	wrefresh(haut);
-	selection = -1;
+      afficheHautMessage("Refresh");
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[0]) || selection == 0){
-	char filename[50];
-        for(int i=0; i<49; i++) filename[i] = '\0';
-        int compteur = 0; 
-        while(compteur<49) {
-          wclear(haut);
-          box(haut, ACS_VLINE, ACS_HLINE);
-          mvwprintw(haut, 1, 1, "Enter file name: %s", filename);
-          move(1, 18+compteur);
-          wrefresh(haut);
-          int ch = getch();
-          if(ch == 10) {
-            filename[compteur] = '\0';
-            break;
-          }
-          else if((ch == 127 || ch == KEY_BACKSPACE) && compteur>0) {
-            compteur--;
-            filename[compteur] = '\0';
-          }
-          else {
-            filename[compteur] = ch;
-            compteur++;
-          }
-        }
-	wclear(bas);
-	chargement(buff, filename);
-	wprintw(bas, "%s", buff->contenu);
-	wrefresh(bas);
+      char filename[50];
+      getFilename(filename);
+      chargement(buff, filename);
+	
+      afficheBufferBas(buff);
+      afficheHautMessageParameterStr("File %s loaded", filename);
 
-	wclear(haut);
-	box(haut, ACS_VLINE, ACS_HLINE);
-	mvwprintw(haut, 1, 1, "File loaded",filename);
-	wrefresh(haut);
-	selection = -1;
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[1]) || selection == 1){
-	char filename[50];
-        for(int i=0; i<49; i++) filename[i] = '\0';
-        int compteur = 0; 
-        while(compteur<49) {
-          wclear(haut);
-          box(haut, ACS_VLINE, ACS_HLINE);
-          mvwprintw(haut, 1, 1, "Enter file name: %s", filename);
-          move(1, 18+compteur);
-          wrefresh(haut);
-          int ch = getch();
-          if(ch == 10) {
-            filename[compteur] = '\0';
-            break;
-          }
-          else if((ch == 127 || ch == KEY_BACKSPACE) && compteur>0) {
-            compteur--;
-            filename[compteur] = '\0';
-          }
-          else {
-            filename[compteur] = ch;
-            compteur++;
-          }
-        }
-	sauvegarde(buff, filename);
-
-	wclear(haut);
-	box(haut, ACS_VLINE, ACS_HLINE);
-	mvwprintw(haut, 1, 1, "File saved in %s",filename);
-	wrefresh(haut);
-	selection = -1;
+      char filename[50];
+      getFilename(filename);
+      sauvegarde(buff, filename);
+      afficheHautMessageParameterStr("File saved in %s", filename);
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[2]) || selection == 2){
-	wclear(haut);
-      	box(haut, ACS_VLINE, ACS_HLINE);
-      	mvwprintw(haut, 1, 1, "Copy");
-	wrefresh(haut);
-	memset(&tmp[0], 0, sizeof(tmp));
-	index = 0;
-	int min = curChar;
-	int max	= buff->cur_char;
-	if(curChar > buff->cur_char){
-		max = curChar;
-		min = buff->cur_char;
-	}
-	while(min < max){
-		tmp[index] = *(buff->contenu + min);
-		min++;
-		index++;
-	}
-	selection = -1;
+      afficheHautMessage("Copy");
+      memset(&tmp[0], 0, sizeof(tmp));
+      index = 0;
+      int min = curChar;
+      int max	= buff->cur_char;
+      if(curChar > buff->cur_char){
+	max = curChar;
+	min = buff->cur_char;
+      }
+      while(min < max){
+	tmp[index] = *(buff->contenu + min);
+	min++;
+	index++;
+      }
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[3]) || selection == 3){
-	wclear(haut);
-      	box(haut, ACS_VLINE, ACS_HLINE);
-	mvwprintw(haut, 1, 1, "Paste");
-	wrefresh(haut);
-	wclear(bas);
-	for (int i = 0; i < strlen(tmp); i++){
-		if(tmp[i] == '\0'){
-			break;
-		}
-		else{
-			insertion(tmp[i], buff);
-		}
+      afficheHautMessage("Paste");
+      for (int i = 0; i < strlen(tmp); i++){
+	if(tmp[i] == '\0'){
+	  break;
 	}
-	wprintw(bas, "%s", buff->contenu);
-	wrefresh(bas);
-	selection = -1;
+	else{
+	  insertion(tmp[i], buff);
+	}
+      }
+
+      afficheBufferBas(buff);
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[4]) || selection == 4){
-	wclear(haut);
-      	box(haut, ACS_VLINE, ACS_HLINE);
-      	mvwprintw(haut, 1, 1, "Cut");
-	wrefresh(haut);
-	wclear(bas);
-    	memset(&tmp[0], 0, sizeof(tmp));
-	index = 0;
-	int min = curChar;
-	int max	= buff->cur_char;
-	int tmpmin = min;
-	if(curChar > buff->cur_char){
-		max = curChar;
-		min = buff->cur_char;
-	}
+      afficheHautMessage("Cut");
+      memset(&tmp[0], 0, sizeof(tmp));
+      index = 0;
+      int min = curChar;
+      int max	= buff->cur_char;
+      int tmpmin = min;
+      if(curChar > buff->cur_char){
+	max = curChar;
+	min = buff->cur_char;
+      }
 	
-	while(tmpmin < max){
-        	tmp[index] = *(buff->contenu + tmpmin);
-       		tmpmin++;
-		index++;
-	}
+      while(tmpmin < max){
+	tmp[index] = *(buff->contenu + tmpmin);
+	tmpmin++;
+	index++;
+      }
 
-	deplacerA(max, buff);
+      deplacerA(max, buff);
 
-	while(min < max){
-		deplacer(-1, buff);
-		supprimer(buff);
-		max--;
-	}
+      while(min < max){
+	deplacer(-1, buff);
+	supprimer(buff);
+	max--;
+      }
     
-	wprintw(bas, "%s", buff->contenu);
-	wrefresh(bas);
-	selection = -1;
+      afficheBufferBas(buff);
+      selection = -1;
       continue;
     }
     else if(ch == CTRL(control[5]) || selection == 5){
-	wclear(haut);
-      	box(haut, ACS_VLINE, ACS_HLINE);
-      	mvwprintw(haut, 1, 1, "Clear");
-	wrefresh(haut);
-	while(true){
-		if(!(*(buff->contenu + (buff->cur_char)) != '\n' && buff->cur_char < buff->dernier))
-			break;
-		deplacer(1, buff);
-		supprimer(buff);		
+      afficheHautMessage("Clear");
+      while(true){
+	if(!(*(buff->contenu + (buff->cur_char)) != '\n' && buff->cur_char < buff->dernier))
+	  break;
+	deplacer(1, buff);
+	supprimer(buff);		
+      }
+      while(true){
+	if(buff->posX != 0 && buff->cur_char > 0){
+	  deplacer(-1, buff);
+	  supprimer(buff);
 	}
-	while(true){
-		if(buff->posX != 0 && buff->cur_char > 0){
-			deplacer(-1, buff);
-			supprimer(buff);
-		}
-		else{
-			break;
-		}
+	else{
+	  break;
 	}
-	wclear(bas);
-	wprintw(bas, "%s", buff->contenu);
-	wrefresh(bas);
-	selection = -1;
+      }
+      afficheBufferBas(buff);
+      selection = -1;
       continue;	
     }
 
@@ -250,23 +231,23 @@ void lancer () {
 	int tmpY = (buff->posY)-1;
 
 	while(buff->posY != tmpY){
-		if(buff->cur_char > 0){
-			deplacer(-1, buff);
-		}else{
-			deplacerA(0, buff);
-			break;
-		}
+	  if(buff->cur_char > 0){
+	    deplacer(-1, buff);
+	  }else{
+	    deplacerA(0, buff);
+	    break;
+	  }
 	}
 
 	int tmpCur_char = buff->cur_char;
 
 	while(buff->posX != tmpX){
-		if(buff->posX > 0){
-			deplacer(-1, buff);
-		}else{
-			deplacerA(tmpCur_char, buff);
-			break;
-		}
+	  if(buff->posX > 0){
+	    deplacer(-1, buff);
+	  }else{
+	    deplacerA(tmpCur_char, buff);
+	    break;
+	  }
 	}
       }
       //wscrl(bas, -1);
@@ -276,20 +257,20 @@ void lancer () {
 	int tmpX = buff->posX;
 	int tmpY = (buff->posY)+1;
 	while(buff->posY != tmpY){
-		if(buff->cur_char < buff->dernier){
-			deplacer(1, buff);
-		}else{
-			deplacerA(buff->dernier, buff);
-			break;
-		}
+	  if(buff->cur_char < buff->dernier){
+	    deplacer(1, buff);
+	  }else{
+	    deplacerA(buff->dernier, buff);
+	    break;
+	  }
 	}
 	
 	while(buff->posX != tmpX){
-		if(*(buff->contenu + (buff->cur_char)) != '\n' && buff->cur_char < buff->dernier){
-			deplacer(1, buff);
-		}else{
-			break;
-		}
+	  if(*(buff->contenu + (buff->cur_char)) != '\n' && buff->cur_char < buff->dernier){
+	    deplacer(1, buff);
+	  }else{
+	    break;
+	  }
 	}
       }
       break;
@@ -297,9 +278,9 @@ void lancer () {
     case KEY_LEFT:
       if(buff->cur_char > 0){
 	if(first == 1){
-		first = 0;
-		wclear(bas);
-		wprintw(bas, "%s", buff->contenu);	
+	  first = 0;
+	  wclear(bas);
+	  wprintw(bas, "%s", buff->contenu);	
 	}
         deplacer(-1,buff);
       }
@@ -308,35 +289,35 @@ void lancer () {
     case KEY_RIGHT:
       if(buff->cur_char < buff->dernier){
 	if(first == 1){
-		first = 0;
-		wclear(bas);
-		wprintw(bas, "%s", buff->contenu);	
+	  first = 0;
+	  wclear(bas);
+	  wprintw(bas, "%s", buff->contenu);	
 	}
         deplacer(1,buff);
       }
       break;
 
     case KEY_SRIGHT:
-	mvwchgat(bas, buff->posY, buff->posX, 1, A_STANDOUT, 1, NULL);
-	if(first == 0){
-		curChar = buff->cur_char;
-		first = 1;
-	}
-	deplacer(1,buff);
-	wrefresh(bas);
-	break;
+      mvwchgat(bas, buff->posY, buff->posX, 1, A_STANDOUT, 1, NULL);
+      if(first == 0){
+	curChar = buff->cur_char;
+	first = 1;
+      }
+      deplacer(1,buff);
+      wrefresh(bas);
+      break;
 
     case KEY_SLEFT:
-	mvwchgat(bas, buff->posY, buff->posX, 1, A_STANDOUT, 1, NULL);
-	if(first == 0){
-		curChar = buff->cur_char;
-		first = 1;
-	}
-	deplacer(-1,buff);
-	wrefresh(bas);
-	break;
+      mvwchgat(bas, buff->posY, buff->posX, 1, A_STANDOUT, 1, NULL);
+      if(first == 0){
+	curChar = buff->cur_char;
+	first = 1;
+      }
+      deplacer(-1,buff);
+      wrefresh(bas);
+      break;
 
-    //Touche Entrée
+      //Touche Entrée
     case 10 :
       if(buff->posX < buff-> t_ligne) {
 	wclear(bas);
@@ -346,13 +327,13 @@ void lancer () {
       break;      
 
     case KEY_DC :
-	if(buff->cur_char < buff->dernier){
-		deplacer(1, buff);
-		deplacer(-1, buff);
-		supprimer(buff);
-		wclear(bas);
-		wprintw(bas, "%s", buff->contenu);
-	}
+      if(buff->cur_char < buff->dernier){
+	deplacer(1, buff);
+	deplacer(-1, buff);
+	supprimer(buff);
+	wclear(bas);
+	wprintw(bas, "%s", buff->contenu);
+      }
       break;
       
     case 127:
--- src/vue.h
+++ src/vue.h
@@ -4,4 +4,34 @@
 #include "menu.h"
 #define CTRL(c) (c & 037)
 
+/*
+ * Donne à filename la valeur entrée par l'utilisateur
+ */
+void getFilename (char* filename);
+
+/*
+ * Rafraichit et affiche un message dans la fenêtre du haut
+ */
+void afficheHautMessage (char* message);
+
+/*
+ * Rafraichit et affiche un message dans la fenêtre du haut
+ * avec le paramètre char* donné en argument
+ */
+void afficheHautMessageParameterStr (char* message, char* parameter);
+
+/*
+ * Rafraichit et affiche un message dans la fenêtre du haut
+ * avec le paramètre int donné en argument
+ */
+void afficheHautMessageParameterInt (char* message, int parameter);
+
+/*
+ * Rafraichit et affiche le contenu du buffer dans la fenêtre du bas
+ */
+void afficheBufferBas (buffer *buff);
+
+/*
+ * Lance le programme
+ */
 void lancer ();
